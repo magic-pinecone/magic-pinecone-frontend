3f8038988af016a467bebec7b96950141528b854
Yuyu1115 <105599462+Yuyu-1115@users.noreply.github.com>
2026-06-07 18:17:55 +0800

    refactor(core): introduce Riverpod providers for app scope dependencies

---
+++ lib/core/app/app_providers.dart
@@ -0,0 +1,159 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:prototype/core/app/app_backend_config.dart';
+import 'package:prototype/core/app/app_theme.dart';
+import 'package:prototype/features/course_selection/data/course_api_service.dart';
+import 'package:prototype/features/course_selection/data/course_repository.dart';
+import 'package:prototype/features/course_selection/data/course_supplemental_detail_catalog.dart';
+import 'package:prototype/features/course_selection/presentation/view_models/course_selection_controller.dart';
+import 'package:prototype/features/home/data/home_dashboard_repository.dart';
+import 'package:prototype/features/home/presentation/view_models/home_view_model.dart';
+import 'package:prototype/features/news/data/news_digest_repository.dart';
+import 'package:prototype/features/news/data/scholarship_api_service.dart';
+import 'package:prototype/features/news/data/scholarship_repository.dart';
+import 'package:prototype/features/news/presentation/view_models/news_view_model.dart';
+import 'package:prototype/features/portal/data/portal_authenticator.dart';
+import 'package:prototype/features/portal/data/portal_shortcut_repository.dart';
+import 'package:prototype/features/portal/presentation/view_models/portal_session_controller.dart';
+import 'package:prototype/features/settings/data/settings_repository.dart';
+import 'package:prototype/features/settings/presentation/view_models/settings_view_model.dart';
+
+part 'app_providers.g.dart';
+
+final appThemeControllerProvider = ChangeNotifierProvider<AppThemeController>((
+  ref,
+) {
+  return AppThemeController();
+});
+
+final appBackendConfigControllerProvider =
+    ChangeNotifierProvider<AppBackendConfigController>((ref) {
+      return AppBackendConfigController();
+    });
+
+@riverpod
+Dio dio(DioRef ref) {
+  final dio = Dio();
+  final config = ref.read(appBackendConfigControllerProvider);
+  dio.options.baseUrl = config.baseUrl;
+
+  ref.listen<AppBackendConfigController>(appBackendConfigControllerProvider, (
+    previous,
+    next,
+  ) {
+    dio.options.baseUrl = next.baseUrl;
+  }, fireImmediately: false);
+  return dio;
+}
+
+@riverpod
+CourseApiService courseApiService(CourseApiServiceRef ref) {
+  final dio = ref.watch(dioProvider);
+  return CourseApiService(dio);
+}
+
+@riverpod
+CourseRepository courseRepository(CourseRepositoryRef ref) {
+  final service = ref.watch(courseApiServiceProvider);
+  return RemoteCourseRepository(service: service);
+}
+
+@riverpod
+CourseSupplementalDetailRepository courseSupplementalDetailRepository(
+  CourseSupplementalDetailRepositoryRef ref,
+) {
+  final dio = ref.watch(dioProvider);
+  return StaticRemoteCourseSupplementalDetailRepository(dio: dio);
+}
+
+@riverpod
+SettingsRepository settingsRepository(SettingsRepositoryRef ref) {
+  return const StaticSettingsRepository();
+}
+
+@riverpod
+HomeDashboardRepository homeDashboardRepository(
+  HomeDashboardRepositoryRef ref,
+) {
+  return const StaticHomeDashboardRepository();
+}
+
+@riverpod
+NewsDigestRepository newsDigestRepository(NewsDigestRepositoryRef ref) {
+  return const StaticNewsDigestRepository();
+}
+
+@riverpod
+PortalShortcutRepository portalShortcutRepository(
+  PortalShortcutRepositoryRef ref,
+) {
+  return const StaticPortalShortcutRepository();
+}
+
+@riverpod
+ScholarshipApiService scholarshipApiService(ScholarshipApiServiceRef ref) {
+  final dio = ref.watch(dioProvider);
+  return ScholarshipApiService(dio);
+}
+
+@riverpod
+ScholarshipRepository scholarshipRepository(ScholarshipRepositoryRef ref) {
+  final service = ref.watch(scholarshipApiServiceProvider);
+  return RemoteScholarshipRepository(service: service);
+}
+
+@riverpod
+PortalAuthenticator portalAuthenticator(PortalAuthenticatorRef ref) {
+  return PortalAuthenticator();
+}
+
+// view models: ChangeNotifiers
+final courseSelectionControllerProvider =
+    ChangeNotifierProvider.autoDispose<CourseSelectionController>((ref) {
+      final repository = ref.watch(courseRepositoryProvider);
+      return CourseSelectionController(repository: repository);
+    });
+
+final settingsViewModelProvider =
+    ChangeNotifierProvider.autoDispose<SettingsViewModel>((ref) {
+      final appThemeController = ref.watch(appThemeControllerProvider);
+      final appBackendConfigController = ref.watch(
+        appBackendConfigControllerProvider,
+      );
+      final repository = ref.watch(settingsRepositoryProvider);
+      return SettingsViewModel(
+        appThemeController: appThemeController,
+        appBackendConfigController: appBackendConfigController,
+        repository: repository,
+      );
+    });
+
+final homeViewModelProvider = ChangeNotifierProvider.autoDispose<HomeViewModel>(
+  (ref) {
+    final repository = ref.watch(homeDashboardRepositoryProvider);
+    return HomeViewModel(repository: repository);
+  },
+);
+
+final newsViewModelProvider = ChangeNotifierProvider.autoDispose<NewsViewModel>(
+  (ref) {
+    final repository = ref.watch(scholarshipRepositoryProvider);
+    final digestRepository = ref.watch(newsDigestRepositoryProvider);
+    return NewsViewModel(
+      repository: repository,
+      digestRepository: digestRepository,
+    );
+  },
+);
+
+final portalSessionControllerProvider =
+    ChangeNotifierProvider.autoDispose<PortalSessionController>((ref) {
+      final authenticator = ref.watch(portalAuthenticatorProvider);
+      final shortcutRepository = ref.watch(portalShortcutRepositoryProvider);
+      return PortalSessionController(
+        authenticator: authenticator,
+        shortcutRepository: shortcutRepository,
+      );
+    });
---
+++ lib/core/app/app_providers.g.dart
@@ -0,0 +1,188 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dioHash() => r'dio';
+
+@ProviderFor(dio)
+final dioProvider = AutoDisposeProvider<Dio>.internal(
+  dio,
+  name: r'dioProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dioHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef DioRef = AutoDisposeProviderRef<Dio>;
+
+String _$courseApiServiceHash() => r'courseApiService';
+
+@ProviderFor(courseApiService)
+final courseApiServiceProvider = AutoDisposeProvider<CourseApiService>.internal(
+  courseApiService,
+  name: r'courseApiServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$courseApiServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CourseApiServiceRef = AutoDisposeProviderRef<CourseApiService>;
+
+String _$courseRepositoryHash() => r'courseRepository';
+
+@ProviderFor(courseRepository)
+final courseRepositoryProvider = AutoDisposeProvider<CourseRepository>.internal(
+  courseRepository,
+  name: r'courseRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$courseRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CourseRepositoryRef = AutoDisposeProviderRef<CourseRepository>;
+
+String _$courseSupplementalDetailRepositoryHash() =>
+    r'courseSupplementalDetailRepository';
+
+@ProviderFor(courseSupplementalDetailRepository)
+final courseSupplementalDetailRepositoryProvider =
+    AutoDisposeProvider<CourseSupplementalDetailRepository>.internal(
+      courseSupplementalDetailRepository,
+      name: r'courseSupplementalDetailRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$courseSupplementalDetailRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef CourseSupplementalDetailRepositoryRef =
+    AutoDisposeProviderRef<CourseSupplementalDetailRepository>;
+
+String _$settingsRepositoryHash() => r'settingsRepository';
+
+@ProviderFor(settingsRepository)
+final settingsRepositoryProvider =
+    AutoDisposeProvider<SettingsRepository>.internal(
+      settingsRepository,
+      name: r'settingsRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$settingsRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef SettingsRepositoryRef = AutoDisposeProviderRef<SettingsRepository>;
+
+String _$homeDashboardRepositoryHash() => r'homeDashboardRepository';
+
+@ProviderFor(homeDashboardRepository)
+final homeDashboardRepositoryProvider =
+    AutoDisposeProvider<HomeDashboardRepository>.internal(
+      homeDashboardRepository,
+      name: r'homeDashboardRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$homeDashboardRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef HomeDashboardRepositoryRef =
+    AutoDisposeProviderRef<HomeDashboardRepository>;
+
+String _$newsDigestRepositoryHash() => r'newsDigestRepository';
+
+@ProviderFor(newsDigestRepository)
+final newsDigestRepositoryProvider =
+    AutoDisposeProvider<NewsDigestRepository>.internal(
+      newsDigestRepository,
+      name: r'newsDigestRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$newsDigestRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef NewsDigestRepositoryRef = AutoDisposeProviderRef<NewsDigestRepository>;
+
+String _$portalShortcutRepositoryHash() => r'portalShortcutRepository';
+
+@ProviderFor(portalShortcutRepository)
+final portalShortcutRepositoryProvider =
+    AutoDisposeProvider<PortalShortcutRepository>.internal(
+      portalShortcutRepository,
+      name: r'portalShortcutRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$portalShortcutRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef PortalShortcutRepositoryRef =
+    AutoDisposeProviderRef<PortalShortcutRepository>;
+
+String _$scholarshipApiServiceHash() => r'scholarshipApiService';
+
+@ProviderFor(scholarshipApiService)
+final scholarshipApiServiceProvider =
+    AutoDisposeProvider<ScholarshipApiService>.internal(
+      scholarshipApiService,
+      name: r'scholarshipApiServiceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$scholarshipApiServiceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef ScholarshipApiServiceRef =
+    AutoDisposeProviderRef<ScholarshipApiService>;
+
+String _$scholarshipRepositoryHash() => r'scholarshipRepository';
+
+@ProviderFor(scholarshipRepository)
+final scholarshipRepositoryProvider =
+    AutoDisposeProvider<ScholarshipRepository>.internal(
+      scholarshipRepository,
+      name: r'scholarshipRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$scholarshipRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef ScholarshipRepositoryRef =
+    AutoDisposeProviderRef<ScholarshipRepository>;
+
+String _$portalAuthenticatorHash() => r'portalAuthenticator';
+
+@ProviderFor(portalAuthenticator)
+final portalAuthenticatorProvider =
+    AutoDisposeProvider<PortalAuthenticator>.internal(
+      portalAuthenticator,
+      name: r'portalAuthenticatorProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$portalAuthenticatorHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef PortalAuthenticatorRef = AutoDisposeProviderRef<PortalAuthenticator>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
